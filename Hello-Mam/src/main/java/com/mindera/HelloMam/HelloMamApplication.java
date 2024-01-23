668b662cc8041357645955d24318ed85fb0bc1f3
tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-01-23 11:18:17 +0000

    project initializers added

---
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/HelloMamApplication.java
@@ -0,0 +1,13 @@
+package com.mindera.HelloMam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMamApplication.class, args);
+	}
+
+}
---
+++ Hello-Mam/src/test/java/com/mindera/HelloMam/HelloMamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindera.HelloMam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
