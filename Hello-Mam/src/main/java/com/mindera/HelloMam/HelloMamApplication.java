ba80bd1e39a008d385ffaeea41dbe27623224eef
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-01-23 19:51:24 +0000

    RestController added to main for testing purpose

--- Hello-Mam/src/main/java/com/mindera/HelloMam/HelloMamApplication.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/HelloMamApplication.java
@@ -2,10 +2,17 @@ package com.mindera.HelloMam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloMamApplication {
 
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Welcome to my app";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloMamApplication.class, args);
 	}
