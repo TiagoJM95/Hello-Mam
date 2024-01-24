46604c8392b6397eed28b6eca61c840e88d5bf11
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-01-24 17:16:14 +0000

    pom.xml beautify

---
+++ hello-grandma/src/main/java/com/mindera/GreetingResource.java
@@ -0,0 +1,18 @@
+package com.mindera;
+
+import io.smallrye.common.annotation.NonBlocking;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @NonBlocking
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+}
