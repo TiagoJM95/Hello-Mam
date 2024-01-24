eab7ebb20d8c9fd8d4de89df8b86fc4b7b38f645
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-01-24 12:35:29 +0000

    More UserCreateDto updates

--- Hello-Mam/src/main/java/com/mindera/HelloMam/dtos/creates/UserCreateDto.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/dtos/creates/UserCreateDto.java
@@ -1,7 +1,25 @@
 package com.mindera.HelloMam.dtos.creates;
 
+import jakarta.persistence.UniqueConstraint;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
-public record UserCreateDto(String username, String name, String email, LocalDate dateOfBirth) {
+public record UserCreateDto(
+        @Valid
+        @NotBlank(message = "Username is mandatory")
+        String username,
+        @Valid
+        @NotBlank(message = "Name is mandatory")
+        String name,
+        @Valid
+        @NotBlank(message = "Email is mandatory")
+        String email,
+        @Valid
+        @NotBlank(message = "Date Of Birth is mandatory")
+        @Past(message = "Date of birth must be in the past")
+        LocalDate dateOfBirth) {
 
 }
