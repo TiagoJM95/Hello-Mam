7bbfb418b9adb07946c4dce1f0eccc960cd20339
c-cunha <car930193@gmail.com>
2024-01-24 23:33:10 +0000

    The controller class is done but hasn't been tested yet. Changes have also been made to the package services classes.

--- Hello-Mam/src/main/java/com/mindera/HelloMam/services/interfaces/MediaService.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/services/interfaces/MediaService.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface MediaService {
 
     List<MediaGetDto> getAllMedia();
+    MediaGetDto getMediaById(Integer id);
     List<MediaGetDto> getMediaByType(MediaType type);
     MediaGetDto getMediaByRefId(String refId);
     MediaGetDto addNewMedia(MediaCreateDto mediaCreateDto);
