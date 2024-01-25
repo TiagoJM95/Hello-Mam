1484a0b24161a8557b0c9304d065ee4ccfc038da
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-01-25 14:58:49 +0000

    Z-Authentication into Z-Auth2

--- Hello-Mam/src/main/java/com/mindera/HelloMam/dtos/gets/UserGetDto.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/dtos/gets/UserGetDto.java
@@ -2,5 +2,5 @@ package com.mindera.HelloMam.dtos.gets;
 
 import java.time.LocalDate;
 
-public record UserGetDto(Long userId, String username, String name, String email, LocalDate dateOfBirth) {
+public record UserGetDto(Long userId, String username, String email, String name, LocalDate dateOfBirth) {
 }
