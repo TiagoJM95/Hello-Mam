62ed81b1e9c4f03f7246396c29a213b066a3d8a9
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-01-26 11:06:09 +0000

    rebase rating-skeleton

--- Hello-Mam/src/main/java/com/mindera/HelloMam/services/interfaces/MediaService.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/services/interfaces/MediaService.java
@@ -3,13 +3,14 @@ package com.mindera.HelloMam.services.interfaces;
 import com.mindera.HelloMam.dtos.creates.MediaCreateDto;
 import com.mindera.HelloMam.dtos.gets.MediaGetDto;
 import com.mindera.HelloMam.enums.MediaType;
+import com.mindera.HelloMam.exceptions.media_exceptions.MediaNotFoundException;
 
 import java.util.List;
 
 public interface MediaService {
 
     List<MediaGetDto> getAllMedia();
-    MediaGetDto getMediaById(Integer id);
+    MediaGetDto getMediaById(Integer id) throws MediaNotFoundException;
     List<MediaGetDto> getMediaByType(MediaType type);
     MediaGetDto getMediaByRefId(String refId);
     MediaGetDto addNewMedia(MediaCreateDto mediaCreateDto);
