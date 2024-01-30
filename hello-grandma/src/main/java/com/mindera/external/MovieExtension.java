6e02ff1fb4eb7cd869b6b81459970699f19a2922
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-01-30 11:47:57 +0000

    API response and conversion working

--- hello-grandma/src/main/java/com/mindera/external/MovieExtension.java
+++ hello-grandma/src/main/java/com/mindera/external/MovieExtension.java
@@ -1,20 +1,39 @@
 package com.mindera.external;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 
 public class MovieExtension {
+
     @JsonProperty("id")
     private long id;
+
     @JsonProperty("title")
     private String title;
-    @JsonProperty("originalLanguage")
+
+    @JsonProperty("original_language")
     private String originalLanguage;
-    @JsonProperty("originalTitle")
+
+    @JsonProperty("original_title")
     private String originalTitle;
+
     @JsonProperty("overview")
     private String overview;
-    @JsonProperty("releaseDate")
+
+    @JsonProperty("release_date")
     private String releaseDate;
-    @JsonProperty("voteAverage")
+
+    @JsonProperty("vote_average")
     private double voteAverage;
 }
