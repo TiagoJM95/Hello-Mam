ed603271495281a44fb4a04373958d32a30e04e7
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-01-30 22:07:51 +0000

    Find By Id external added

---
+++ hello-grandma/src/main/java/com/mindera/external/MovieDetails.java
@@ -0,0 +1,30 @@
+package com.mindera.external;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class MovieDetails {
+    private Long id;
+    private String title;
+    private String release_date;
+    private Double vote_average;
+    private String overview;
+    private String status;
+    private String tagline;
+    private Integer runtime;
+    private String original_language;
+    private String revenue;
+    private String budget;
+    private String popularity;
+    private String vote_count;
+    private List<Object> genres;
+}
