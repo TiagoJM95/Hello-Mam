deb9216d74a4eccb047f66169bce46b2e44694f4
c-cunha <car930193@gmail.com>
2024-01-31 17:26:08 +0000

    exception handler done

--- hello-grandma/src/main/java/com/mindera/util/Messages.java
+++ hello-grandma/src/main/java/com/mindera/util/Messages.java
@@ -4,4 +4,8 @@ public class Messages {
     public static String MOVIE_NOT_FOUND = "Movie not found";
     public static String VIDEOGAME_NOT_FOUND = "Videogame not found";
     public static String MUSIC_NOT_FOUND = "Music not found";
+    public static String MOVIE_RELATED_ERROR = "Movie-related error: ";
+    public static String MUSIC_RELATED_ERROR = "Music-related error: ";
+    public static String VIDEO_GAME_RELATED_ERROR = "Video game-related error: ";
+    public static String INTERNAL_SERVER_ERROR = "Internal Server error: ";
 }
