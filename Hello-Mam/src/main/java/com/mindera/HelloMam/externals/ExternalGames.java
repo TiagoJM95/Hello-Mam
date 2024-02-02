06555ac1e7a303052eb2fa54ab545faae75d6f1b
José Ruy Sá Ribeiro <22319070+jruysaribeiro@users.noreply.github.com>
2024-02-02 15:36:20 +0000

    MediaController Update

--- Hello-Mam/src/main/java/com/mindera/HelloMam/externals/ExternalGames.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/externals/ExternalGames.java
@@ -12,9 +12,57 @@ public class ExternalGames {
     String uriBase = "http://localhost:8080/api/v1/videogames";
 
     @Cacheable("games")
-    public String getAllGames(){
+    public String getAllVideogames(){
         return restClient.get()
                 .uri(uriBase + "/videogames")
+                .retrieve()
+                .body(String.class);
+    }
+
+    @Cacheable("games")
+    public String getGameById(String refId){
+        return restClient.get()
+                .uri(uriBase + "/videogames/" + refId)
+                .retrieve()
+                .body(String.class);
+    }
+
+    @Cacheable("games")
+    public String getGameDetails(Integer refId){
+        return restClient.get()
+                .uri(uriBase + "/videogames/" + refId)
+                .retrieve()
+                .body(String.class);
+    }
+
+    @Cacheable("games")
+    public String getGameBySearch(String search){
+        return restClient.get()
+                .uri(uriBase + "/search/" + search)
+                .retrieve()
+                .body(String.class);
+    }
+
+    @Cacheable("games")
+    public String getGameByGenre(int genre){
+        return restClient.get()
+                .uri(uriBase + "/genres/" + genre)
+                .retrieve()
+                .body(String.class);
+    }
+
+    @Cacheable("games")
+    public String getGameRecommendations(Integer refId){
+        return restClient.get()
+                .uri(uriBase + "/recommendations/" + refId)
+                .retrieve()
+                .body(String.class);
+    }
+
+    @Cacheable("games")
+    public String getDiscoverGame(int i) {
+        return restClient.get()
+                .uri(uriBase + "/discover/" + i)
                 .retrieve()
                 .body(String.class);
     }
