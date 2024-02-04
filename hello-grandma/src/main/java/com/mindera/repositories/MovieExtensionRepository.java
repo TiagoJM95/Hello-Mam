00d5e41e0c8bbe71dec42c0d58f1b9eeaa0f94dd
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-04 12:09:14 +0000

    MovieExtensionServiceImpl deprecated. MovieServiceImpl injects ExtensionRepo

--- hello-grandma/src/main/java/com/mindera/repositories/MovieExtensionRepository.java
+++ hello-grandma/src/main/java/com/mindera/repositories/MovieExtensionRepository.java
@@ -5,8 +5,6 @@ import com.mindera.entities.MovieExtension;
 import jakarta.ws.rs.*;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import java.util.List;
-
 @Path("/3")
 @RegisterRestClient(configKey="tmdb-api")
 public interface MovieExtensionRepository {
@@ -16,30 +14,34 @@ public interface MovieExtensionRepository {
 
     @GET
     @Path("/movie/{movie_id}")
-    Movie getMovieById(@PathParam("movie_id") String movieId,
-                       @HeaderParam("accept") String acceptHeader,
-                       @QueryParam("api_key") String authorizationHeader);
+    Movie getMovieDetailsByTmdbId(
+            @PathParam("movie_id") String movieId,
+            @HeaderParam("accept") String acceptHeader,
+            @QueryParam("api_key") String authorizationHeader);
 
     @GET
     @Path("/movie/{movie_id}/recommendations")
-    MovieExtension getMovieRecommendation(@PathParam("movie_id") Integer movieId,
-                                          @HeaderParam("accept") String acceptHeader,
-                                          @QueryParam("api_key") String authorizationHeader);
+    MovieExtension getMovieRecommendationByTmdbId(
+            @PathParam("movie_id") Integer movieId,
+            @HeaderParam("accept") String acceptHeader,
+            @QueryParam("api_key") String authorizationHeader);
     @GET
     @Path("/discover/movie")
-    MovieExtension discoverMovies(@QueryParam("page") Integer page,
-                              @QueryParam("sort_by") String sortBy,
-                              @QueryParam("vote_count.gte") Integer voteCount,
-                              @QueryParam("with_original_language") String withOriginalLanguage,
-                              @QueryParam("with_genres") String withGenres,
-                              @HeaderParam("accept") String acceptHeader,
-                              @QueryParam("api_key") String authorizationHeader);
+    MovieExtension discoverMoviesWithFilters(
+            @QueryParam("page") Integer page,
+            @QueryParam("sort_by") String sortBy,
+            @QueryParam("vote_count.gte") Integer voteCount,
+            @QueryParam("with_original_language") String withOriginalLanguage,
+            @QueryParam("with_genres") String withGenres,
+            @HeaderParam("accept") String acceptHeader,
+            @QueryParam("api_key") String authorizationHeader);
 
     @GET
     @Path("/search/movie")
-    MovieExtension findMovieByTitle(@QueryParam("query") String query,
-                            @QueryParam("language") String language,
-                            @QueryParam("page") Integer page,
-                            @HeaderParam("accept") String acceptHeader,
-                            @QueryParam("api_key") String authorizationHeader);
+    MovieExtension findMovieByTitle(
+            @QueryParam("query") String query,
+            @QueryParam("language") String language,
+            @QueryParam("page") Integer page,
+            @HeaderParam("accept") String acceptHeader,
+            @QueryParam("api_key") String authorizationHeader);
 }
