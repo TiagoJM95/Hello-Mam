02e66473d3d414f465091ba02e4d9678fa7da9bd
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-04 12:09:14 +0000

    MovieExtensionServiceImpl deprecated. MovieServiceImpl injects ExtensionRepo

--- hello-grandma/src/main/java/com/mindera/deprecated/MovieExtensionServiceImpl.java
+++ hello-grandma/src/main/java/com/mindera/deprecated/MovieExtensionServiceImpl.java
@@ -1,3 +1,4 @@
+/*
 package com.mindera.services.implementations;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -8,7 +9,6 @@ import com.mindera.repositories.MovieExtensionRepository;
 import com.mindera.services.interfaces.MovieExtensionService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.util.List;
@@ -29,31 +29,39 @@ public class MovieExtensionServiceImpl implements MovieExtensionService {
 
     @Override
     public Movie getMovieDetailsByTmdbId(String tmdbId) throws JsonProcessingException {
-        /*String json = movieExtensionRepository.getMovieById(tmdbId, MediaType.APPLICATION_JSON, API_KEY);
-        return mapper.readValue(json, Movie.class);*/
-        return movieExtensionRepository.getMovieById(tmdbId, APPLICATION_JSON, API_KEY);
+        */
+/*String json = movieExtensionRepository.getMovieById(tmdbId, MediaType.APPLICATION_JSON, API_KEY);
+        return mapper.readValue(json, Movie.class);*//*
+
+        return movieExtensionRepository.getMovieDetailsByTmdbId(tmdbId, APPLICATION_JSON, API_KEY);
     }
 
     @Override
     public List<MovieExtension.MovieResponse> getMovieRecommendation(Integer movieId) throws JsonProcessingException {
-        /*String json = movieExtensionRepository.getMovieRecommendation(movieId, ACCEPT_HEADER, API_KEY);
-        return mapper.readValue(json, MovieExtension.class).getResults();*/
-        return movieExtensionRepository.getMovieRecommendation(movieId, ACCEPT_HEADER, API_KEY).getResults();
+        */
+/*String json = movieExtensionRepository.getMovieRecommendation(movieId, ACCEPT_HEADER, API_KEY);
+        return mapper.readValue(json, MovieExtension.class).getResults();*//*
+
+        return movieExtensionRepository.getMovieRecommendationByTmdbId(movieId, ACCEPT_HEADER, API_KEY).getResults();
     }
 
     @Override
     public List<MovieExtension.MovieResponse> discoverMovies(String genres) throws JsonProcessingException {
-        /*String json = movieExtensionRepository.discoverMovies(1, "vote_average.desc", 100,
+        */
+/*String json = movieExtensionRepository.discoverMovies(1, "vote_average.desc", 100,
                 "en", genres, ACCEPT_HEADER, API_KEY);
-        return mapper.readValue(json, MovieExtension.class).getResults();*/
-        return movieExtensionRepository.discoverMovies(1, "vote_average.desc", 100,
+        return mapper.readValue(json, MovieExtension.class).getResults();*//*
+
+        return movieExtensionRepository.discoverMoviesWithFilters(1, "vote_average.desc", 100,
                 "en", genres, APPLICATION_JSON, API_KEY).getResults();
     }
 
     @Override
     public List<MovieExtension.MovieResponse> findMovieByTitle(String title) throws JsonProcessingException {
-        /*String json = movieExtensionRepository.findMovieByTitle(title, "en-US", 1, ACCEPT_HEADER, API_KEY);
-        return mapper.readValue(json, MovieExtension.class).getResults();*/
+        */
+/*String json = movieExtensionRepository.findMovieByTitle(title, "en-US", 1, ACCEPT_HEADER, API_KEY);
+        return mapper.readValue(json, MovieExtension.class).getResults();*//*
+
         return movieExtensionRepository.findMovieByTitle(title, "en-US", 1, APPLICATION_JSON, API_KEY).getResults();
     }
-}
+}*/
