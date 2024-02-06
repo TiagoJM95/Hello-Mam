e22abfb43a6628294a89365b8c355492eafd8942
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-06 23:55:34 +0000

    docker-compose.yml working

--- Hello-Mam/src/main/java/com/mindera/HelloMam/HelloMamApplication.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/HelloMamApplication.java
@@ -6,24 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 @EnableCaching
 public class HelloMamApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloMamApplication.class, args);
 	}
-
-	/*hello-mam:
-	build: ./Hello-Mam
-	container_name: hello-mam
-	ports:
-			- "8080:8080"
-
-	hello-grandma:
-	build: ./hello-grandma
-	container_name: hello-grandma
-	ports:
-			- "8082:8080"*/
-
 }
