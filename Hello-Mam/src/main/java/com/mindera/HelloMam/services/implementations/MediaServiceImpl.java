d9548074bfe04e72746fdfeac380a3d01a9effe5
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-07 14:49:01 +0000

    Added cacheable to some MediaServiceImpl methods

--- Hello-Mam/src/main/java/com/mindera/HelloMam/services/implementations/MediaServiceImpl.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/services/implementations/MediaServiceImpl.java
@@ -6,10 +6,8 @@ import com.mindera.HelloMam.exceptions.MediaTypeNotFoundException;
 import com.mindera.HelloMam.exceptions.media.*;
 import com.mindera.HelloMam.entities.Media;
 import com.mindera.HelloMam.enums.MediaType;
-import com.mindera.HelloMam.externals.clients.ExternalGameClient;
 import com.mindera.HelloMam.externals.models.ExternalGame;
 import com.mindera.HelloMam.externals.models.ExternalMovie;
-import com.mindera.HelloMam.externals.clients.ExternalMovieClient;
 import com.mindera.HelloMam.repositories.MediaRepository;
 import com.mindera.HelloMam.services.interfaces.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +16,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static com.mindera.HelloMam.converters.MediaConverter.fromMediaEntityToMediaGetDto;
 import static com.mindera.HelloMam.enums.MediaType.*;
 
 @Service
@@ -31,22 +28,26 @@ public class MediaServiceImpl implements MediaService {
         this.mediaRepository = mediaRepository;
     }
 
+    @Cacheable("media")
     @Override
     public List<MediaGetDto> getAllMedia() {
         return mediaRepository.findAll().stream().map(MediaConverter::fromMediaEntityToMediaGetDto).toList();
     }
 
+    @Cacheable("media")
     @Override
     public MediaGetDto getMediaById(Long id) throws MediaNotFoundException {
         return MediaConverter.fromMediaEntityToMediaGetDto(findMediaById(id));
     }
 
+    @Cacheable("media")
     @Override
     public List<MediaGetDto> getMediaByType(String type) throws MediaTypeNotFoundException {
         MediaType mediaType = MediaType.getTypeByDescription(type).orElseThrow(MediaTypeNotFoundException::new);
         return mediaRepository.findByMediaType(mediaType).stream().map(MediaConverter::fromMediaEntityToMediaGetDto).toList();
     }
 
+    @Cacheable("media")
     @Override
     public MediaGetDto getMediaByRefId(Long refId) throws RefIdNotFoundException {
         Media media = mediaRepository.findByRefId(refId).orElseThrow(RefIdNotFoundException::new);
