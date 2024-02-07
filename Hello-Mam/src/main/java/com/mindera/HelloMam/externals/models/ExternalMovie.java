fd4b0b1712f467686532c6d4b0fc96bbd9b4db44
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-07 15:50:27 +0000

    Added Serializable to ExternalMovie. Deleted unused properties. Added @JsonIgnoreProperties(ignoreUnknown = true)

--- Hello-Mam/src/main/java/com/mindera/HelloMam/externals/models/ExternalMovie.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/externals/models/ExternalMovie.java
@@ -1,56 +1,33 @@
 package com.mindera.HelloMam.externals.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import java.io.Serializable;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class ExternalMovie {
-
+public class ExternalMovie implements Serializable {
     private Long tmdbId;
-
-
     private String title;
-
     @JsonProperty("release_date")
     private String releaseDate;
-
     @JsonProperty("vote_average")
     private Double voteAverage;
-
-
     private String overview;
-
-
     private String status;
-
-
     private String tagline;
-
-
     private Integer runtime;
-
     @JsonProperty("original_language")
     private String originalLanguage;
-
-
     private String revenue;
-
-    /*@JsonProperty("budget")
-    private String budget;
-
-    @JsonProperty("popularity")
-    private String popularity;
-
-    @JsonProperty("vote_count")
-    private String voteCount;*/
-
     @JsonProperty("genres")
     private List<String> genres;
 }
