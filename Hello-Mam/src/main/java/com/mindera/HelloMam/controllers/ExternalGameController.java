edbe0a8d64eaa6b173f93d111a86346110ae51e9
Tiago <150834373+TiagoJM95@users.noreply.github.com>
2024-02-08 15:27:01 +0000

    Swagger annotations beginning

--- Hello-Mam/src/main/java/com/mindera/HelloMam/controllers/ExternalGameController.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/controllers/ExternalGameController.java
@@ -1,25 +1,25 @@
 package com.mindera.HelloMam.controllers;
 
-import com.mindera.HelloMam.exceptions.media.RefIdNotFoundException;
 import com.mindera.HelloMam.externals.models.ExternalGame;
 import com.mindera.HelloMam.services.implementations.ExternalGameServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Games", description = "The Games API")
 @RestController
-@CrossOrigin
 @RequestMapping("api/v1/games")
 public class ExternalGameController {
 
@@ -30,66 +30,58 @@ public class ExternalGameController {
         this.externalGameService = externalGameService;
     }
 
-    @Operation(summary = "Get all videogames.", description = "Returns a list of all videogames available in the database.")
-    @ApiResponse(responseCode = "200", description = "List of videogames available in the database.",
-            content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExternalGame.class))})
+    @Operation(
+            summary = "Get all games",
+            description = "Get all games from the Grandma API",
+            tags = {"Games", "get"}
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of games",
+            content = {@Content(schema = @Schema(implementation = ExternalGame.class), mediaType = "application/json")})
     @GetMapping("/")
     public ResponseEntity<List<ExternalGame>> getAllVideogames(){
         return ResponseEntity.ok(externalGameService.getAllVideogames());
     }
 
-    @Operation(summary = "Get a videogame by its id.")
+    @Operation(
+            summary = "Get a game by id",
+            description = "Get a ExternalGame object by specifying its id. The response is ExternalGame object with id, name, release date, rating and a list of it's genres.",
+            tags = {"Games", "get", "id"}
+    )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Videogame found.",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExternalGame.class))}),
-            @ApiResponse(responseCode = "404", description = "Videogame not found.",
-                    content = @Content)})
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "ExternalGame object",
+                    content = {@Content(schema = @Schema(implementation = ExternalGame.class), mediaType = "application/json")}),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "The ExternalGame object with the specified id was not found.",
+                    content = {@Content(schema = @Schema())})
+    })
+    @Parameter(name = "id",description = "Provide a ExternalGame ID to be searched", required = true)
     @GetMapping("/id/{id}")
     public ResponseEntity<ExternalGame> getVideogameById(@PathParam("id") int id){
         return ResponseEntity.ok(externalGameService.getVideogameById(id));
     }
 
-    @Operation(summary = "Get videogames by title.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Videogames found.",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExternalGame.class))}),
-            @ApiResponse(responseCode = "404", description = "Videogames not found.",
-                    content = @Content)})
     @GetMapping("/title/{title}")
-    public ResponseEntity<List<ExternalGame>> getGameByTitle(@PathParam("title") String title) throws RefIdNotFoundException {
+    public ResponseEntity<List<ExternalGame>> getGameByTitle(@PathParam("title") String title) {
         return ResponseEntity.ok(externalGameService.getGameByTitle(title));
     }
 
-    @Operation(summary = "Get videogames recommendations based on a provided game.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Videogames found.",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExternalGame.class))}),
-            @ApiResponse(responseCode = "404", description = "Videogames not found.",
-                    content = @Content)})
     @GetMapping("/recommendations/{id}")
-    public ResponseEntity<List<ExternalGame>> getGameRecommendations(@PathParam("id") int id) throws RefIdNotFoundException {
+    public ResponseEntity<List<ExternalGame>> getGameRecommendations(@PathParam("id") int id) {
         return ResponseEntity.ok(externalGameService.getGameRecommendations(id));
     }
 
-    @Operation(summary = "Get videogames by genre.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Videogames found.",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExternalGame.class))}),
-            @ApiResponse(responseCode = "404", description = "Videogames not found.",
-                    content = @Content)})
     @GetMapping("/genre/{genre}")
-    public ResponseEntity<List<ExternalGame>> getGameByGenre(@PathParam("genre") String genre) throws RefIdNotFoundException {
+    public ResponseEntity<List<ExternalGame>> getGameByGenre(@PathParam("genre") String genre) {
         return ResponseEntity.ok(externalGameService.getGameByGenre(genre));
     }
 
-    @Operation(summary = "Get the top 5 videogames.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Top 5 videogames found.",
-                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExternalGame.class))}),
-            @ApiResponse(responseCode = "404", description = "Top 5 videogames not found.",
-                    content = @Content)})
     @GetMapping("/top")
-    public ResponseEntity<List<ExternalGame>> getTopFiveVideoGames() throws RefIdNotFoundException {
+    public ResponseEntity<List<ExternalGame>> getTopFiveVideoGames() {
         return ResponseEntity.ok(externalGameService.getTopFiveVideoGames());
     }
 
--- Hello-Mam/src/main/java/com/mindera/HelloMam/externals/models/ExternalGame.java
+++ Hello-Mam/src/main/java/com/mindera/HelloMam/externals/models/ExternalGame.java
@@ -2,6 +2,7 @@ package com.mindera.HelloMam.externals.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import java.io.Serializable;
 import java.util.List;
@@ -13,11 +14,17 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 
+@Schema(name = "ExternalGame", description = "ExternalGame Entity Information")
 public class ExternalGame implements Serializable {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "ExternalGame Id", example = "123")
     private Long igdbId;
+    @Schema(description = "ExternalGame's title", example = "Elden Ring")
     private String name;
+    @Schema(description = "ExternalGame's release date", example = "2024-02-09")
     private String releaseDate;
+    @Schema(description = "ExternalGame's rating", example = "9.5")
     private Double rating;
     @JsonProperty("genreIds")
+    @Schema(name = "genres", description = "ExternalGame's genres", example = "[\"Adventure\", \"RPG\"]")
     private List<String> genres;
 }
